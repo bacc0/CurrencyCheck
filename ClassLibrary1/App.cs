10a7e9701d2504caa71bfdf6445f5c4c96967704
Veselin Hr. Trayanov <1.1@gmx.com>
2018-12-21 17:09:39 +0000

    Update App.cs

--- ClassLibrary1/App.cs
+++ ClassLibrary1/App.cs
@@ -14,18 +14,44 @@ namespace ClassLibrary1
             var input = "https://bacc0.github.io/One.html"; //     https://api.coinmarketcap.com/v2/listings/
 
             var jsonString = Read.ReadFromWeb(input);
-            
+
             var jsonObject = JsonConvert.DeserializeObject<CurrencyDataAndMetadata>(jsonString);
 
             foreach (var currency in jsonObject.data)
             {
                 var id = currency.Id;
 
-                Ids.iDsTest(iDs, id);
+                Ids.IdsTest(iDs, id);
                 Name.NameTest(currency.Name, id);
                 Symbol.SymbolTest(currency.Symbol, id);
-                WebsiteSlug.WebsiteSlugTest(currency.Website_slug, id);          
+                WebsiteSlug.WebsiteSlugTest(currency.Website_slug, id);
             }
+
+//            var num = int.Parse(Console.ReadLine());
+//
+//            var idDoesExist = true;
+//
+//            foreach (var currencyData in jsonObject.data)
+//            {
+//
+//                if (currencyData.Id == num)
+//                {
+//                    Console.WriteLine("-------------------------------\n" +
+//                                      currencyData.Name + "\n" +
+//                                      currencyData.Id + "\n" +
+//                                      currencyData.Symbol + "\n" +
+//                                      currencyData.Website_slug + "\n" +
+//                                      "-------------------------------");
+//                    idDoesExist = false;
+//                    break;
+//                }
+//            }
+//
+//            if (idDoesExist)
+//            {
+//                Console.WriteLine($"The ID ({num}) does not exist in the current context!\n");
+//            }
+//            Console.WriteLine($" Total type of currencys ({jsonObject.data.Count}) \n-------------------------------");
         }
     }
 }
